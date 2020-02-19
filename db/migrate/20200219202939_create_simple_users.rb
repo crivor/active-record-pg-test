5da0eb2fa998bd8847839ad90f074798700a3a6a
Steven Foster <steven@stevenfoster.net>
2020-02-19 12:41:46 -0800

    adding columns for migration

--- db/migrate/20200219202939_create_simple_users.rb
+++ db/migrate/20200219202939_create_simple_users.rb
@@ -1,6 +1,8 @@
 class CreateSimpleUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :simple_users do |t|
+      t.string :name
+      t.integer :age
 
       t.timestamps
     end
